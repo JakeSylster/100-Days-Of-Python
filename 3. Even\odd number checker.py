16550f4d9e4998284c29277a5347d79af1f1eb04
Ajay Golla <44961432+JakeSylster@users.noreply.github.com>
2025-04-28 13:26:57 -0400

    Create 3. Even\odd number checker.py

---
+++ 3. Even\odd number checker.py
@@ -0,0 +1,7 @@
+#Even or Odd number checker
+print("Welcome to even or odd number checker")
+number = int(input("Please enter the number to check"))
+if number % 2 == 0:
+    print("The number is Even")
+else:
+    print("The number is Odd")
