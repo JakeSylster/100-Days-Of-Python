576cd66c2ec91361d797804949fc5c24267bd19d
Ajay Golla <44961432+JakeSylster@users.noreply.github.com>
2026-04-13 12:37:34 -0400

    Implement basic snake class with movement controls

---
+++ Day 20/snake.py
@@ -0,0 +1,42 @@
+from  turtle import Turtle
+
+UP = 90
+DOWN = 270
+LEFT = 180
+RIGHT = 0
+
+#Create a snake with three blocks
+class Snake:
+    def __init__(self):
+        self.snake = []
+        self.create_snake()
+        self.head = self.snake[0]
+
+    def create_snake(self):
+        x = 0
+        for _ in range(3):
+            snake_segments = Turtle("square")
+            snake_segments.color("white")
+            snake_segments.penup()
+            snake_segments.goto(x,0)
+            x -= 20
+
+            self.snake.append(snake_segments)
+
+    def move(self):
+        for segments in range(len(self.snake) - 1 ,0,-1):
+            self.snake[segments].goto(self.snake[segments-1].pos())
+        self.head.forward(20)
+
+    def up(self):
+        if self.head.heading() != DOWN:
+            self.head.setheading(UP)
+    def left(self):
+        if self.head.heading() != RIGHT:
+            self.head.setheading(LEFT)
+    def down(self):
+        if self.head.heading() != UP:
+            self.head.setheading(DOWN)
+    def right(self):
+        if self.head.heading() != LEFT:
+            self.head.setheading(RIGHT)
